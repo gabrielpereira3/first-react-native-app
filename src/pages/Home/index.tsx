72e401a643bff01aa81bea5dcf53209f7c17ad4a
Gabriel <gabriel.soares14563@gmail.com>
2022-09-13 11:19:53 -0300

    Separando componente de Task List

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,9 +7,8 @@ import {
   SafeAreaView,
   TextInput,
   TouchableOpacity,
-  ScrollView,
-  FlatList,
 } from 'react-native';
+import {TaskList} from '../../components/TaskList';
 
 interface Task {
   id: string;
@@ -50,15 +49,7 @@ export const Home = () => {
 
         <Text style={styles.titleTasks}>Minhas tarefas</Text>
 
-        <FlatList
-          data={tasks}
-          keyExtractor={item => item.id}
-          renderItem={({item}) => (
-            <TouchableOpacity style={styles.buttonTask}>
-              <Text style={styles.titleTask}>{item.title}</Text>
-            </TouchableOpacity>
-          )}
-        />
+        <TaskList tasks={tasks} />
       </View>
     </SafeAreaView>
   );
@@ -104,18 +95,6 @@ const styles = StyleSheet.create({
   buttonText: {
     color: '#121214',
     fontSize: 18,
-    fontWeight: 'bold',
-  },
-  buttonTask: {
-    backgroundColor: '#29292e',
-    padding: 10,
-    marginTop: 10,
-    borderRadius: 50,
-    alignItems: 'center',
-  },
-  titleTask: {
-    color: '#f1f1f1',
-    fontSize: 20,
     fontWeight: 'bold',
   },
 });
