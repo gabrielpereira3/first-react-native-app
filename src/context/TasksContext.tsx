9420be91fc18ab6f57d5698177a0b5a37d890029
Gabriel <gabriel.soares14563@gmail.com>
2022-09-13 15:21:19 -0300

    Mudanças

--- src/context/TasksContext.tsx
+++ src/context/TasksContext.tsx
@@ -4,9 +4,14 @@ interface IProps {
   children: React.ReactElement;
 }
 
-export interface ITasksContext {
+export interface ITask {
   id: string;
   title: string;
+}
+
+export interface ITasksContext {
+  tasks: ITask[];
+  addTask(task: ITask): void;
 }
 
 export const TasksContext = React.createContext<ITasksContext>(
@@ -14,8 +19,14 @@ export const TasksContext = React.createContext<ITasksContext>(
 );
 
 export const TasksProvider: React.FunctionComponent<IProps> = ({children}) => {
+  const addTask = (task: ITask) => {
+    console.log('Add task action');
+  };
+
+  const tasks = [{id: '1', title: 'Task01'}];
+
   return (
-    <TasksContext.Provider value={{id: '1', title: 'Task01'}}>
+    <TasksContext.Provider value={{tasks, addTask}}>
       {children}
     </TasksContext.Provider>
   );
