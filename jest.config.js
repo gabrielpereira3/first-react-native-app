e1f631531d445f50292c4c767f24be0cf9c2c3ee
Gabriel <gabriel.soares14563@gmail.com>
2022-09-14 15:24:51 -0300

    Configuração personalizada do jest

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  testPathIgnorePatterns: ['/node_modules', '/android', '/ios'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
