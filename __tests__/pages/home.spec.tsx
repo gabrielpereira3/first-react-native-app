66736c4bd841facac08cfd9cc75d4e9bb9deb819
Gabriel <gabriel.soares14563@gmail.com>
2022-09-14 14:43:29 -0300

    Testando a página Home

---
+++ __tests__/pages/home.spec.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {fireEvent, render} from '@testing-library/react-native';
+import {renderHook, act} from '@testing-library/react-hooks';
+import {TasksProvider, useTaskList} from '../../src/context/TasksContext';
+import {Home} from '../../src/pages/Home';
+
+describe('Home page', () => {
+  it('renders correctly', () => {
+    const {getByPlaceholderText} = render(<Home />);
+
+    const inputNewTask = getByPlaceholderText('Nova tarefa...');
+
+    expect(inputNewTask).toBeDefined();
+    expect(inputNewTask.props.placeholder).toBeTruthy();
+  });
+
+  it('verifica a inserção de um item na lista de tarefas', async () => {
+    const {result} = renderHook(() => useTaskList(), {
+      wrapper: TasksProvider,
+    });
+
+    const data = {id: 'Task01', title: 'Task01'};
+
+    await act(() => result.current.addTask(data));
+
+    expect(result.current.tasks).toBeTruthy();
+  });
+
+  it('verifica o clique no botão insere um item na lista de tarefas', async () => {
+    const {getByPlaceholderText, getByTestId} = render(<Home />, {
+      wrapper: TasksProvider,
+    });
+
+    const {result} = renderHook(() => useTaskList(), {
+      wrapper: TasksProvider,
+    });
+
+    const inputNewTask = getByPlaceholderText('Nova tarefa...');
+    const button = getByTestId('addButton');
+
+    const data = {id: 'Task01', title: 'Task01'};
+
+    act(() => fireEvent.changeText(inputNewTask, data.title));
+
+    await act(async () => {
+      await fireEvent.press(button);
+    });
+
+    expect(result.current.tasks).toBeTruthy();
+  });
+});
