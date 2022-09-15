a6e698204c0dc0bd3ffc07ddf4606ad76c3dd446
Gabriel Pereira Soares <gabriel.soares14563@gmail.com>
2022-09-14 21:28:29 -0300

    Teste mais produtivo e relatório dos testes

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   testPathIgnorePatterns: ['/node_modules', '/android', '/ios'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.tsx', '!src/**/*spec.tsx'],
+  coverageReporters: ['lcov'],
 };
